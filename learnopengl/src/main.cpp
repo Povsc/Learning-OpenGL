4198df51d7f0948830c174da8c09b6cb51208b7c
Povsc <felipeconte0@gmail.com>
2023-07-19 20:46:56 -0400

    Can create window

--- learnopengl/src/main.cpp
+++ learnopengl/src/main.cpp
@@ -1,2 +1,63 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+void draw(GLFWwindow* window) {
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+}
+
+void framebuffer_size_callback(GLFWwindow* window,  int width, int height) {
+	glViewport(0, 0, width, height);
+
+	// updates frames while resizing window (performant?)
+	draw(window);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_CORE_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "FAILED TO CREATE GLFW WINDOW\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "FAILED TO INITIALIZE GLAD\n";
+			return -1;
+	}
+
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		// input
+		processInput(window);
+
+		//rendering and swapping buffers
+		draw(window);
+		
+		// check and call events
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
